f165a28e9aa1f0c0648f4d9325a23226e6c48d00
Freerk <freerk@Freerks-MacBook-Air.local>
2021-12-23 20:03:28 +0100

    random algorythm

--- script.js
+++ script.js
@@ -1,4 +1,9 @@
-function showTheText() {
-    document.getElementById("answer").classList.remove("hidden")
-    document.getElementById("button").classList.add("hidden")
-  }
+const memes = ["lindner_kapitalist.png"];
+
+const filename = memes[Math.floor(Math.random() * memes.length)];
+document.getElementById("image").src = `./img/${filename}`;
+
+function show() {
+  document.getElementById("answer").classList.remove("hidden");
+  document.getElementById("button").classList.add("hidden");
+}
